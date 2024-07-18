fabb3d593c5b5a2bcb23a603a650f5f43fd57ef5
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-07-18 12:33:54 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,10 +6,11 @@ def main():
     connection, _ = server_socket.accept()
 
     request = connection.recv(1024).decode().split("\r\n")[0].split(" ")
+    endpoint, string = request[1].split("/")
     response = ""
 
-    if request[1] == "/":
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+    if endpoint == "echo":
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {string}\r\n"
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     connection.send(response.encode())
