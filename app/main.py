4e5ca0ab0c0dfd30eaaf93ac281ff1b2b35ec632
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-07-18 12:11:46 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import socket
 
 
 def main():
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, _ = server_socket.accept()  # wait for client
+    connection, _ = server_socket.accept()
 
+    request = connection.recv(1024).decode()
+    print(request)
     response = "HTTP/1.1 200 OK\r\n\r\n"
     connection.send(response.encode())
 
