f2535032ff995802baba67c4b13a0c3919397435
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-07-18 13:02:34 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, _ = server_socket.accept()
 
-    request = connection.recv(1024).decode().split("\r\n")[0].split(" ")
-    path = request[1]
+    request = connection.recv(1024).decode().split("\r\n")
+    path = request[0].split(" ")[1]
     response = ""
 
     if "echo" in path:
@@ -14,6 +14,10 @@ def main():
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}"
     elif path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
+    elif path == "/user-agent":
+        print(request[3])
+        user_agent = request[3].split()[1]
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     connection.send(response.encode())
