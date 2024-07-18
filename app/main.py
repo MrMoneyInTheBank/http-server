4d48097b08ecde3d63c1d5fad1e614fb7a73ef72
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-07-18 12:30:13 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, _ = server_socket.accept()
 
-    request = connection.recv(1024).decode()
-    print(request)
-    response = "HTTP/1.1 200 OK\r\n\r\n"
+    request = connection.recv(1024).decode().split("\r\n")[0].split(" ")
+    response = ""
+
+    if request[1] == "/":
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
     connection.send(response.encode())
 
 
