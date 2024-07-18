f0492998b71febaa719850a8e9e78fcc78815aa5
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2024-07-18 12:09:16 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    connection, _ = server_socket.accept()  # wait for client
+
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    connection.send(response.encode())
 
 
 if __name__ == "__main__":
